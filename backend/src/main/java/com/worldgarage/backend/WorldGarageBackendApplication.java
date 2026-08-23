1ae834f21777dab7a607ddc99eb7741641478642
Sun Shuo <ssun430@aucklanduni.ac.nz>
2026-08-23 16:28:22 +0800

    chore: scaffold Spring Boot backend

---
+++ backend/src/main/java/com/worldgarage/backend/WorldGarageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.worldgarage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldGarageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldGarageBackendApplication.class, args);
+	}
+
+}
