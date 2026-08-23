04153e95895171f6028a3344946d5be6f4c31359
Sun Shuo <ssun430@aucklanduni.ac.nz>
2026-08-23 21:03:08 +0800

    feat: expose car lookup endpoint

--- backend/src/main/java/com/worldgarage/backend/controller/CarController.java
+++ backend/src/main/java/com/worldgarage/backend/controller/CarController.java
@@ -3,7 +3,10 @@ package com.worldgarage.backend.controller;
 import com.worldgarage.backend.model.Car;
 import com.worldgarage.backend.service.CarService;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,16 @@ public class CarController {
   @GetMapping
   public List<Car> getCars() {
     return carService.getAllCars();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Car> getCarById(@PathVariable long id) {
+    Optional<Car> car = carService.getCarById(id);
+
+    if (car.isPresent()) {
+      return ResponseEntity.ok(car.get());
+    }
+
+    return ResponseEntity.notFound().build();
   }
 }
